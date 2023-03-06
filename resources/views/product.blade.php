c18a2f2652bd9d6408130c3b3d2bee9f39847b7f
Georgopulos Andreas <andreas.georgopulos@kryonet.hu>
2023-03-06 17:58:56 +0100

    Product page build

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,13 +4,19 @@
 		<h1 class="font-russo-one color-orange">{{$model->getTitle()}}</h1>
 
 		<div class="content">
-			<p class="font-exo-2 color-gray">{!! $model->getLead() !!}</p>
-			<p class="font-exo-2 color-gray">
-				@if($model->hasIndexImage())
-					<img src="{{$model->getIndexImageFileUrl('page')}}" alt="{{$model->getTitle()}}" />
-				@endif
-			</p>
-			<p class="font-exo-2 color-gray">{!! $model->getBody() !!}</p>
+			<div class="row">
+				<div class="col-sm-6">
+					@if($model->hasIndexImage())
+						<img src="{{$model->getIndexImageFileUrl('page')}}" class="img-fluid img-thumbnail" alt="{{$model->getTitle()}}" />
+					@endif
+				</div>
+				<div class="col-sm-6 font-exo-2 color-gray">{!! $model->getLead() !!}</div>
+			</div>
+
+			<div class="row">
+				<div class="col-12 font-exo-2 color-gray">{!! $model->getBody() !!}</div>
+			</div>
+
 			<p class="font-exo-2 color-gray">€{!! number_format($model->price, 2, '.', ',') !!}</p>
 
 			<div id="add-to-cart"
