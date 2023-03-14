a4609ddaed50f59f2c735abe2d8d3cf9adc8fdd6
Georgopulos Andreas <andreas.georgopulos@kryonet.hu>
2023-03-14 19:10:01 +0100

    Remove fa icons from menu items.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,12 +13,12 @@
                 <ul class="navbar-nav ms-auto mb-2 mb-md-0">
                     <li class="nav-item ml-auto">
                         <a class="nav-link @if(url()->current() === url('/')) active @endif" href="{{url('/')}}#products">
-                            <i class="fa fa-trademark"></i> {{trans('Products')}}
+                            {{trans('Products')}}
                         </a>
                     </li>
                     <li class="nav-item ml-auto">
                         <a class="nav-link @if(url()->current() === url('/page/terms-and-conditions')) active @endif" href="{{url(route('page', ['slug' => 'terms-and-conditions']))}}">
-                            <i class="fa fa-check"></i> {{trans('Terms and conditions')}}
+                            {{trans('Terms and conditions')}}
                         </a>
                     </li>
                     <li class="nav-item ml-auto">
