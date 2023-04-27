41335bcf16959690aa85a91d7a21c38b40cda8b9
Georgopulos Andreas <andreas.georgopulos@kryonet.hu>
2023-04-27 20:07:56 +0200

    Set index video.

--- resources/assets/js/indexVideo.js
+++ resources/assets/js/indexVideo.js
@@ -1,16 +1,26 @@
 (function () {
-	const video = document.querySelector('.video-background-holder video');
+	const videoContainer = document.querySelector('.video-background-holder');
+	const video = videoContainer.querySelector('video');
 
 	function resizeVideo() {
 		const { innerWidth, innerHeight } = window;
 		const videoRatio = video.videoWidth / video.videoHeight;
 		const windowRatio = innerWidth / innerHeight;
+		const containerRatio = videoContainer.offsetWidth / videoContainer.offsetHeight;
 		if (windowRatio < videoRatio) {
 			video.style.width = 'auto';
 			video.style.height = '100%';
+			if ((videoContainer.offsetHeight * videoRatio) < videoContainer.offsetWidth) {
+				video.style.width = '100%';
+				video.style.height = 'auto';
+			}
 		} else {
 			video.style.width = '100%';
 			video.style.height = 'auto';
+			if ((videoContainer.offsetWidth / videoRatio) < videoContainer.offsetHeight) {
+				video.style.width = 'auto';
+				video.style.height = '100%';
+			}
 		}
 	}
 
