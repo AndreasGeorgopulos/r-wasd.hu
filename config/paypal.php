f370b47817df4917e51d6c75df90f5b4232e88dd
Georgopulos Andreas <andreas.georgopulos@kryonet.hu>
2023-05-16 21:42:10 +0200

    Test Paypal live

--- config/paypal.php
+++ config/paypal.php
@@ -5,7 +5,7 @@ return [
 	/*'client_id' => 'AbtnNlHxh11LSArDbvox94kl1kwXPUDbMklFirHY_kfv3XnbGoB1_zdxKqXVvjFO4qzQEXWdSv2Qf9j9',
 	'secret' => 'ELZo2rc7YaakIlhhc-S4btzXULsNbPBTT3oYEh5r0IAyBEQ9OJGnjjIsUQa-qnkAS2TpSEmger1o507Q',*/
 	'settings' => [
-		'mode' => 'sandbox',
+		'mode' => 'live',
 		'http.ConnectionTimeOut' => 1000,
 		'log.LogEnabled' => true,
 		'log.FileName' => storage_path() . '/logs/paypal.log',
