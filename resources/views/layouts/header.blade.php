443a4b43746b0e2dd5d7225ba1a347acb034dfe7
Georgopulos Andreas <andreas.georgopulos@kryonet.hu>
2023-05-23 22:07:16 +0200

    Add arrow links to header menu.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,21 +12,37 @@
             <div class="collapse navbar-collapse justify-content-sm-center" id="navbarCollapse">
                 <ul class="navbar-nav ms-auto mb-2 mb-md-0">
                     <li class="nav-item ml-auto text-end text-md-start">
+                        <a class="pull-left nav-link @if(url()->current() === url('/')) active @endif" href="{{url('/')}}#products">
+                            <i class="fa fa-chevron-right"></i>
+                        </a>
+
                         <a class="nav-link @if(url()->current() === url('/')) active @endif" href="{{url('/')}}#products">
                             {{trans('Products')}}
                         </a>
                     </li>
                     <li class="nav-item ml-auto text-end text-md-start">
+                        <a class="pull-left nav-link @if(url()->current() === url('/page/about-us')) active @endif" href="{{url(route('page', ['slug' => 'about-us']))}}">
+                            <i class="fa fa-chevron-right"></i>
+                        </a>
+
                         <a class="nav-link @if(url()->current() === url('/page/about-us')) active @endif" href="{{url(route('page', ['slug' => 'about-us']))}}">
                             {{trans('About us')}}
                         </a>
                     </li>
                     <li class="nav-item ml-auto text-end text-md-start">
+                        <a class="pull-left nav-link @if(url()->current() === url('/contact')) active @endif" href="{{url(route('contact'))}}">
+                            <i class="fa fa-chevron-right"></i>
+                        </a>
+
                         <a class="nav-link @if(url()->current() === url('/contact')) active @endif" href="{{url(route('contact'))}}">
                             <i class="fa fa-envelope"></i> {{trans('Contact')}}
                         </a>
                     </li>
                     <li class="nav-item ml-auto text-end text-md-start">
+                        <a class="pull-left nav-link @if(url()->current() === url('/cart')) active @endif" href="{{url('/cart')}}" tabindex="-1" aria-disabled="true">
+                            <i class="fa fa-chevron-right"></i>
+                        </a>
+
                         <a class="nav-link @if(url()->current() === url('/cart')) active @endif" href="{{url('/cart')}}" tabindex="-1" aria-disabled="true">
                             <i class="fa fa-shopping-cart"></i> {{trans('Cart')}}
                         </a>
